e86e3a9f87e41ac4858b9254ac1d0f0d92348980
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-08-19 12:38:07 -0300

    creating file to generate access.log

---
+++ src/ingest/create-file-log.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from "node:crypto";
+import { createWriteStream } from "node:fs";
+
+const stream = createWriteStream("./src/database/access.log");
+
+for (let i = 1; i <= 5; i++) {
+  stream.write(
+    JSON.stringify({
+      id: randomUUID(),
+      ip: `192.168.0.${i}`,
+      username: `username${i}`,
+      first_name: `First Name ${i}`,
+      last_name: `Last Name ${i}`,
+      email: `user${i}@example.com`,
+      location: `Location ${i}`,
+      job_area: `Job Area ${i}`,
+      company: `Company ${i}`,
+      job_title: `Job Title ${i}`,
+      timestamp: new Date().toISOString(),
+    }) + "\n",
+  );
+}
+
+stream.end(() => {
+  console.log("Log file has been created and written to successfully.");
+});
